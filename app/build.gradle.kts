7bcc1ec228b8e55eed20bd2cafcf5181f30db593
rsl23 <raoulkho05@gmail.com>
2025-06-29 23:49:36 +0700

    Revert "Filter Part 1"
    This reverts commit 12a9d486b507d9ead851283c2ac96cb3fdb5671a.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -123,7 +123,4 @@ dependencies {
 
     // Untuk handle deep links dari Midtrans callback
     implementation("androidx.browser:browser:1.7.0")
-
-    implementation("androidx.compose.material:material-icons-extended-android:1.7.0") // Check for the latest version
-
 }
